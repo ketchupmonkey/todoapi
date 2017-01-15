df011cf354571d456e256a6d214bccbf5d18e8fe
Shim <shim@node.node>
2017-01-15 21:32:46 +1300

    stuff

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express= require('express');
+var bodyParser=require('body-parser');
+
 var app = express();
 var PORT =process.env.PORT || 3000;
-var todos=[{
-  id: 1,
-  description: 'Meet mom for lunch',
-  completed: false
-},{
-  id:2,
-  description: 'Go to market',
-  completed: false
-},{
-  id:3,
-  description: 'Study nodejs',
-  completed: true
-}
-];
+var todos=[];
+var todoNextId=1;
+
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
   res.send('Todo API Root');
@@ -43,6 +35,21 @@ app.get('/todos/:id',function(req,res){
   }
 
   res.send('Asking for todo with id of '+  req.params.id + todos[req.params.id]);
+});
+
+//POST /todos
+
+app.post('/todos',function(req,res){
+  var body = req.body;
+  // add id field
+  //push body into array
+  body.id=todoNextId;
+  todoNextId ++;
+  //console.log('description: ' + body.description);
+  todos.push(body);
+  //console.log(todos);
+  //console.log(todoNextId);
+  res.json(body);
 });
 
 app.listen(PORT,function(){
